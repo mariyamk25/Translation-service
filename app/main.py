00a8999df3244cbe86b0d7c95a1f04f7b424ceb3
mariyamk25 <mariyam@treatmentgps.com>
2025-06-13 10:09:51 -0400

    edits

--- app/main.py
+++ app/main.py
@@ -1,23 +1,25 @@
-from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
-from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi.templating import Jinja2Templates
 import schemas
-import crud
-import models
+from fastapi import FastAPI, BackgroundTasks, HTTPException, Depends, Request, status
+from fastapi.responses import HTMLResponse
+from sqlalchemy.orm import Session
+from fastapi.templating import Jinja2Templates
+from schemas import TranslationRequest
+import  crud
 from database import get_db, engine
-
+import models
+from fastapi.middleware.cors import CORSMiddleware
+from utily import perform_translation 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-templates= Jinja2Templates(directory= "templates")
+templates =  Jinja2Templates(directory= "templates")
 
 @app.get('/index', response_class=HTMLResponse)
-def index(request: Request):   
-    return templates.TemplateResponse("index.html", {"request": request}) 
-async def read_index(request: Request):
+def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,12 +27,31 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+) 
 
 @app.post("/translate", response_model=schemas.TaskResponse)
-def translate(request: schemas.TranslationRequest):
-
+def translate(request: schemas.TranslationRequest, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+    print("str log-1", request.text, request.languages)
     task = crud.create_translation_task(db, request.text, request.languages)
-    background_tasks.add_task(perform_translation, task.id, request.text, request.languages, db)   
-    return{"task_id": {task.id}}
+    print(task.id)
+    background_tasks.add_task(perform_translation, task.id, request.text, request.languages, db)
+
+    return {"task_id":task.id}
+
+@app.get("/translate/{task_id}", response_model=schemas.TranslationStatus)
+def get_translate(task_id: int, db: Session = Depends(get_db)):
+
+    task = crud.get_translation_task(db, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="task not found")
+    return {"task_id": task_id, "status": task.status, "translation": task.translations}
+
+@app.get("/translate/content/{task_id}", response_model=schemas.TranslationStatus)
+def get_translate_content(task_id: int, db: Session = Depends(get_db)):
+
+    task = crud.get_translation_task(db, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="task not found")
+    return {task}
+
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,12 @@
 from pydantic import BaseModel
 from typing import List, Dict
 
-
 class TranslationRequest(BaseModel):
-    text: str 
+    text: str
     languages: List[str]
-
 class TaskResponse(BaseModel):
     task_id: int
 class TranslationStatus(BaseModel):
     task_id: int
     status: str
-    translation: Dict[str,str]
+    translations: Dict[str, str]    
